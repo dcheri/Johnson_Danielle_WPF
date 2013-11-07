d75f21ad4fcc9be56921d0aaada5331c9d04d626
dcheri <dcheri@fullsail.edu>
2013-11-07 17:47:16 -0600

    Finished completing the Concatenating Strings Screencast.

--- ExpressionsScreencast/js/script.js
+++ ExpressionsScreencast/js/script.js
@@ -1,11 +1,8 @@
-// Expressions - PEMDAS - The Order of Operations
+// Expressions - Concatenating Strings
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var a = "6";
+var b = "7";
 
-//the average is the sum of the items divided by the number of items
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+var result = a + " " + b;
 
-//console.log(average);
+console.log(result);
