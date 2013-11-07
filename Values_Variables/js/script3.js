cc2269c783d03c0ad4f388171f0c10334d5ed3d2
dcheri <dcheri@fullsail.edu>
2013-11-06 22:26:07 -0600

    Added script3.js file. Finished following along with Screencast: String and Boolean Variables.

---
+++ Values_Variables/js/script3.js
@@ -0,0 +1,9 @@
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 38; //Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Danielle"; //String Variable
+
+var prhase = "I don't know \n You'll have to speak to the professor"
