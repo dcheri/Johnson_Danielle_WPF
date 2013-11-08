b1b49036108e91c709ce167839131c55672666ac
dcheri <dcheri@fullsail.edu>
2013-11-07 21:50:24 -0600

    Added and defined variables.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,6 +1,9 @@
 // Danielle Johnson | November 7, 2013 | Expressions_Personal
 
-var girlCount = prompt("How many girls are you buying gifts for this year?");
-var boyCount = prompt("How many boys are you buying gifts for this year?");
-var giftCount = prompt("How many gifts are you buying per person?");
-var giftPrice = prompt("What is the most you want to spend on each gift?");
+var girlCount = prompt("How many girls are you buying gifts for this year?"); //Defines the girlCount variable
+var boyCount = prompt("How many boys are you buying gifts for this year?"); //Defines the boyCount variable
+var giftCount = prompt("How many gifts are you buying per person?"); //Defines the giftCount variable
+var giftPrice = prompt("What is the most you want to spend on each gift?"); //Defines the giftPrice variable
+
+var totalGifts = (girlCount + boyCount) * giftCount; //Calculates total Christmas haul count
+var totalPrice = totalGifts * giftPrice //Calculates total Christmas haul price
