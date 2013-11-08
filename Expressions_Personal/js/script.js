0b28d4bb04497d1c4ba4b2e8bee2966eb10ae49a
dcheri <dcheri@fullsail.edu>
2013-11-07 22:47:05 -0600

    Debugged the errors in the script.js file.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,8 +5,7 @@ var boyCount = prompt("How many boys are you buying gifts for this year?"); //De
 var giftCount = prompt("How many gifts are you buying per person?"); //Defines the giftCount variable
 var giftPrice = prompt("What is the most you want to spend on each gift?"); //Defines the giftPrice variable
 
-var totalGifts = (girlCount + boyCount) * giftCount; //Calculates total Christmas haul count
+var totalGifts = (Number(girlCount) + Number(boyCount)) * giftCount; //Calculates total Christmas haul count
 var totalPrice = totalGifts * giftPrice; //Calculates total Christmas haul price
 
-console.log("The total gifts in your Christmas haul: " + totalGifts "\n" "The cost of your Christmas haul is " + totalPrice + "dollars."); //Print solutions it out the console
-alert("The total gifts in your Christmas haul: " + totalGifts "\n" "The cost of your Christmas haul is " + totalPrice + "dollars."); //Print solutions it out the console
+console.log("You have " + totalGifts + " gifts in your Christmas haul and the cost is " + totalPrice + " dollars."); //Print solutions it out the console
