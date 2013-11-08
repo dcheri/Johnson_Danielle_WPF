9f36a19de9d4dcefef79e90032ae6670975cf729
dcheri <dcheri@fullsail.edu>
2013-11-07 21:29:06 -0600

    Updated Christmas Budget Calculator.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,6 @@
 // Danielle Johnson | November 7, 2013 | Expressions_Personal
 
-var
+var girlCount = prompt("How many girls are you buying gifts for this year?");
+var boyCount = prompt("How many boys are you buying gifts for this year?");
+var giftCount = prompt("How many gifts are you buying per person?");
+var giftPrice = prompt("What is the most you want to spend on each gift?");
