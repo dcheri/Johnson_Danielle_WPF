7ae72fc9425cef9388cafeeb92a9f3175bb1ffef
dcheri <dcheri@fullsail.edu>
2013-11-08 09:42:27 -0600

    Completed the Industry Expressions. Checked for bugs. Corrected spacing error.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,9 @@
 // Danielle Johnson | November 7, 2013 | Expressions_Industry
 
-var 
+var pageCount = prompt("How many pages are in the document to be printed?"); //Asks the user how many pages they have
+var copyCount = prompt("How many copies need to be printed?"); //Asks the user how many copies they need
+var pageCost = prompt("How much is being charged per page?"); //Asks the user how much they are being charged for each page
+
+var finalCost = (Number(pageCount) * Number(copyCount)) * Number(pageCost); //Calculates the information taken above
+
+console.log("The cost of your copy job is $" + finalCost); //Prints the total cost to the console
