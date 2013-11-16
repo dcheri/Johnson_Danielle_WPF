e2178bcd5e359421e8b355531fafb999042a5a2b
dcheri <dcheri@fullsail.edu>
2013-11-15 21:26:46 -0600

    Defined the variables for the conditional statements.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,5 @@
-// Danielle Johnson
+// Danielle Johnson | November 15, 2013 | Conditionals_Personal
+
+var schoolDay = true; //Is today a school day? - Defines whether today is a weekday or weekend
+var jacksBehavior = prompt("Did Jack behave in school today? \(Please answer true or false\)");
+var jillsBehavior = prompt("Did Jill behave in school today? \(Please answer true or false\)");
