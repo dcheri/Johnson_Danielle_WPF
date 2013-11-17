352f36ec92e0099d3653cbc8be1eec6df251042e
dcheri <dcheri@fullsail.edu>
2013-11-16 19:15:38 -0600

    Added files and folders for Industru Conditionals. Also defined variables after initla set up.

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,10 @@
+// Danielle Johnson | Nov. 16, 2013 | Conditionals_Industry
+
+var newVisitor = prompt("Are you a current customer? \(Please answer yes or no.\)");
+var signUp = prompt("Would you like to sign up for our newsletter? \(Please answer yes or no.\)");
+
+if( signUp === "" || newVisitor === ""){
+	alert("You forgot to enter something!");
+}
+
+(signUp === "yes" && newVisitor === "yes") ? console.log("Awesome! Thanks for being loyal and signing up!") : console.log("Hmm, you should stop by more often."")
