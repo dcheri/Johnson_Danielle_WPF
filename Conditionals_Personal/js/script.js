51417fdb7904ea6372779c8e961af8de7a535bd9
dcheri <dcheri@fullsail.edu>
2013-11-16 18:11:47 -0600

    Debugged the error message. Changed '=' to '===' for the expressions.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,13 +1,13 @@
 // Danielle Johnson | November 15, 2013 | Conditionals_Personal
 
 var schoolDay = true; //Is today a school day? - Defines whether today is a weekday or weekend
-var jacksBehavior = prompt("Did Jack behave in school today? \(Please answer true or false\)"); //Defines ja
-var jillsBehavior = prompt("Did Jill behave in school today? \(Please answer true or false\)");
-var iceCreamParty = console.log("Yay! We're going to have ice cream!"); //Let's us know we're having ice cream after school by printing to the console
-var sorryCharlie = console.log("Maybe next time, we\'re going home."); //Let's us know we're headed home after school by printing to the console
+var jacksBehavior = prompt("Did Jack behave in school today? \(Please answer yes or no\)"); //Defines ja
+var jillsBehavior = prompt("Did Jill behave in school today? \(Please answer yes or no\)");
+//var iceCreamParty = console.log("Yay! We're going to have ice cream!"); //Let's us know we're having ice cream after school by printing to the console
+//var sorryCharlie = console.log("Maybe next time, we\'re going home."); //Let's us know we're headed home after school by printing to the console
 
-if (jacksBehavior = "true" && jillsBehavior = "true"){
-	console.log("iceCreamParty");
-} else{
-	console.log("sorryCharlie");
+if(jacksBehavior === "yes" && jillsBehavior === "yes"){
+	console.log("Yay! We're going to have ice cream!"); //Let's us know we're having ice cream after school by printing to the console
+}else{
+	console.log("Maybe next time, we\'re going home."); //Let's us know we're headed home after school by printing to the console
 }
