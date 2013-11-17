76949a081ef599f840ccb9891f3127386d8e38ed
dcheri <dcheri@fullsail.edu>
2013-11-16 18:49:40 -0600

    Added conditional statement to determine naughty or nice list status.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,7 +3,10 @@
 var christmasSpirit = prompt("Do you have the Christmas spirit? \(Please answer yes or no.\)"); //Asks the user if they have Christmas spirit
 var yourNaughty = prompt("On a scale of 1-10 \(10 being the worst\) how naughty were you this year?"); //Asks the user how naughty they've been
 var yourNice = prompt("On a scale of 1-10 \(1 being an angel\) how nice were you this year?"); //Asks the user how nice they've been
-var theResult = (yourNaughty + yourNice)/2; //Calculates your naughty to nice score
-var niceList = console.log("Congratulations, you're on the nice list!")
-var naughtyList = console.log("So Sorry, you're on the naughty list!")
+var theResult = (Number(yourNaughty) + Number(yourNice))/2; //Calculates your naughty to nice score
 
+if(theResult < 5){
+	console.log("Congratulations, you're on the nice list!");
+}else{
+	console.log("So Sorry, you're on the naughty list!");
+}
