b4a3514a46ba05bcc60b04652566c39aa1693d33
dcheri <dcheri@fullsail.edu>
2013-11-16 18:35:24 -0600

    Defined the variables for the conditional statements.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,9 @@
 // Danielle Johnson | Nov. 16, 2013 | Conditionals_Wacky
+
+var christmasSpirit = prompt("Do you have the Christmas spirit? \(Please answer yes or no.\)"); //Asks the user if they have Christmas spirit
+var yourNaughty = prompt("On a scale of 1-10 \(10 being the worst\) how naughty were you this year?"); //Asks the user how naughty they've been
+var yourNice = prompt("On a scale of 1-10 \(1 being an angel\) how nice were you this year?"); //Asks the user how nice they've been
+var theResult = (yourNaughty + yourNice)/2; //Calculates your naughty to nice score
+var niceList = console.log("Congratulations, you're on the nice list!")
+var naughtyList = console.log("So Sorry, you're on the naughty list!")
+
