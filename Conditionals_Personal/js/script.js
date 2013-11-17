186fad41758b46d8072af1200dd4e98b6b9346ec
dcheri <dcheri@fullsail.edu>
2013-11-16 18:14:06 -0600

    Added comments to each line, defining what it does.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,12 +1,12 @@
 // Danielle Johnson | November 15, 2013 | Conditionals_Personal
 
 var schoolDay = true; //Is today a school day? - Defines whether today is a weekday or weekend
-var jacksBehavior = prompt("Did Jack behave in school today? \(Please answer yes or no\)"); //Defines ja
-var jillsBehavior = prompt("Did Jill behave in school today? \(Please answer yes or no\)");
+var jacksBehavior = prompt("Did Jack behave in school today? \(Please answer yes or no\)"); //Defines jacks behavior based on what the user enters
+var jillsBehavior = prompt("Did Jill behave in school today? \(Please answer yes or no\)"); //Defines jills behavior based on what the user enters
 //var iceCreamParty = console.log("Yay! We're going to have ice cream!"); //Let's us know we're having ice cream after school by printing to the console
 //var sorryCharlie = console.log("Maybe next time, we\'re going home."); //Let's us know we're headed home after school by printing to the console
 
-if(jacksBehavior === "yes" && jillsBehavior === "yes"){
+if(jacksBehavior === "yes" && jillsBehavior === "yes"){ //If Jack and Jill BOTH have good behavion, then we'll go have ice cream
 	console.log("Yay! We're going to have ice cream!"); //Let's us know we're having ice cream after school by printing to the console
 }else{
 	console.log("Maybe next time, we\'re going home."); //Let's us know we're headed home after school by printing to the console
