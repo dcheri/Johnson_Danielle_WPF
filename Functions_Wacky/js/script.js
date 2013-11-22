35bd6099f28505f17104e25d2adefc78455f67d0
dcheri <dcheri@fullsail.edu>
2013-11-22 08:51:28 -0600

    Adding variables and defining them.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,8 +1,8 @@
-// Danielle Johnson | November 21, 2013 | Functions_Personal
+// Danielle Johnson | November 21, 2013 | Functions_Wacky
 
 
-var blackFridayPrice = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
-	function dealFinder(originalPrice, percentDiscount) { //Tells the function called dealFinder that we'll be passing two variables to it
+var dogFoodCost = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
+	function dealFinder(daysPerYear, cupsPerDay, costPerCup) { //Tells the function called dealFinder that we'll be passing two variables to it
 		var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount)/100)); //The discount price calculation
 		return finalPrice; //Returns the value
 	}
