76b67dd9ad26b02c82b376d6f87b581d850b908e
dcheri <dcheri@fullsail.edu>
2013-11-22 09:44:29 -0600

    Working through the function, adding necessary variables and conditions.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,10 +1,11 @@
 // Danielle Johnson | November 21, 2013 | Functions_Wacky
 
 
-var dogFoodCost = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
-	function dealFinder(daysPerYear, cupsPerDay, costPerCup) { //Tells the function called dealFinder that we'll be passing two variables to it
-		var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount)/100)); //The discount price calculation
-		return finalPrice; //Returns the value
+var dogFoodCost = foodCalculator (prompt("Is this a leap year? \(Please answer true or false.\)"), prompt("How many cups of food does your dog eat per day? \(XX\)"), prompt("Estimate how much each cup of food costs. \($X.XX\))")); //Defines the dog food cost variable and gets the users input for the type of year, how many cups are fed each day, and how much each cup costs
+	function foodCalculator (daysPerYear, cupsPerDay, costPerCup) { //Tells the function called foodCalculator that we'll be passing three variables to it
+		(daysPerYear == "true") ? var daysPerYear = 366 : var daysPerYear = 365;
+		var annuallPrice = daysPerYear - (originalPrice * ( Number(percentDiscount)/100)); //The discount price calculation
+		return annualPrice; //Returns the value
 	}
 
 console.log("The sale price of your item is $" + Number(blackFridayPrice) + "."); //Prints the result to the console
