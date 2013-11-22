51bf963ed048df8161e66211e76b8dbfc23b2112
dcheri <dcheri@fullsail.edu>
2013-11-22 08:34:40 -0600

    Changed Folder names

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,10 @@
+// Danielle Johnson | November 21, 2013 | Functions_Personal
+
+
+var blackFridayPrice = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
+	function dealFinder(originalPrice, percentDiscount) { //Tells the function called dealFinder that we'll be passing two variables to it
+		var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount)/100)); //The discount price calculation
+		return finalPrice; //Returns the value
+	}
+
+console.log("The sale price of your item is $" + Number(blackFridayPrice) + "."); //Prints the result to the console
