26b3c06d34d5d5ab419a10921d359b0080875cfe
dcheri <dcheri@fullsail.edu>
2013-11-22 11:47:27 -0600

    Adjusted variable names and added conditional.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,14 +1,16 @@
 // Danielle Johnson | November 21, 2013 | Functions_Industry
 
 
-var posterPrice = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
+var finalPrice = posterPrice (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
 	function dealFinder(originalPrice, percentDiscount) { //Tells the function called dealFinder that we'll be passing two variables to it
 		
 
 
-		var pageSize = function (l, h){ //The discount price calculation
-			var area = l * w;
-			return area; //Returns the value
-		}
+
 
 console.log("The sale price of your item is $" + Number(posterPrice) + "."); //Prints the result to the console
+
+var priceDiscount = function (posterPrice, discount){ //The discount price calculation
+	var extraSavings = posterPrice - (posterPrice * ( Number(discount)/100));
+	return extraSavings; //Returns the value
+}
