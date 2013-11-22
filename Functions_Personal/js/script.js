de5b97d1bc842c0fadfb2172f8630c6e9df12ba1
dcheri <dcheri@fullsail.edu>
2013-11-21 23:12:50 -0600

    Having problems with my results. Keep receiving $NaN error.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,9 +1,13 @@
 // Danielle Johnson | November 21, 2013 | Functions_Personal
 
+
+var blackFridayPrice = dealFinder (originalPrice, percentDiscount);
+	function dealFinder() {
+		var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount)/100));
+		return finalPrice;
+	}
+
 var originalPrice = prompt("What is the original price of the item? \($xx.xx\)");
 var percentDiscount = prompt("What is the percentage off the original price of the item? \(XX\))");
 
-var blackFridayPrice = function dealFinder ( originalPrice, percentDiscount) {
-	var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount) / 100 ));
-	return finalPrice;
-}
+console.log("The sale price of your item is $" + Number(blackFridayPrice) + ".");
