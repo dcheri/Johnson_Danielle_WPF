148231306b9858ac94db22f4d93571484dd18c17
dcheri <dcheri@fullsail.edu>
2013-11-21 22:16:02 -0600

    Added a function to calculate the sale price of an item.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,6 @@
 // Danielle Johnson | November 21, 2013 | Functions_Personal
+
+function dealFinder ( originalPrice, percentDiscount) {
+	var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount) / 100 ));
+	
+}
