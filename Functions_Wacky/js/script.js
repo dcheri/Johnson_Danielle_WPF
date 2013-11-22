0c4d77e9b5b09b3a2f73758ee0a7ea550e6ae20c
dcheri <dcheri@fullsail.edu>
2013-11-22 10:05:52 -0600

    Debugged the final errors, by removing "var" from within the tertiary condition

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,9 +3,9 @@
 
 var dogFoodCost = foodCalculator (prompt("Is this a leap year? \(Please answer true or false.\)"), prompt("How many cups of food does your dog eat per day? \(XX\)"), prompt("Estimate how much each cup of food costs. \($X.XX\))")); //Defines the dog food cost variable and gets the users input for the type of year, how many cups are fed each day, and how much each cup costs
 	function foodCalculator (daysPerYear, cupsPerDay, costPerCup) { //Tells the function called foodCalculator that we'll be passing three variables to it
-		(daysPerYear == "true") ? var daysPerYear = 366 : var daysPerYear = 365;
-		var annuallPrice = daysPerYear - (originalPrice * ( Number(percentDiscount)/100)); //The discount price calculation
+		(daysPerYear === "true") ? daysPerYear = 366 : daysPerYear = 365; //This determines if the current year is a leap year or not
+		var annualPrice = Number(daysPerYear) * Number(cupsPerDay) * Number(costPerCup); //The discount price calculation
 		return annualPrice; //Returns the value
 	}
 
-console.log("The sale price of your item is $" + Number(blackFridayPrice) + "."); //Prints the result to the console
+console.log("The cost to feed your dog for a year is $" + Number(dogFoodCost) + "."); //Prints the result to the console
