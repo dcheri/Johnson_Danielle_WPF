fb9142c963b148b835febc2afb1a0b914755e45d
dcheri <dcheri@fullsail.edu>
2013-11-22 13:32:35 -0600

    Added one final if statement with a conditional operator.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,6 +4,13 @@
 var blackFridayPrice = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))")); //Defines the black Friday price variable and gets the users price and discount amount for the calculation
 	function dealFinder(originalPrice, percentDiscount) { //Tells the function called dealFinder that we'll be passing two variables to it
 		var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount)/100)); //The discount price calculation
+		
+		if (percentDiscount > 40){
+			alert("You got a pretty good deal!");
+		} else{
+			alert("You probably could have found a better deal!");
+		}
+
 		return finalPrice; //Returns the value
 	}
 
