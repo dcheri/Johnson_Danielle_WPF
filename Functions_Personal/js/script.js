293660fe5dd635b53987a852435e43517a6c8b74
dcheri <dcheri@fullsail.edu>
2013-11-21 23:21:22 -0600

    Figured out the issue in the code. Misplaced arguments. Problem fixed.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,13 +1,10 @@
 // Danielle Johnson | November 21, 2013 | Functions_Personal
 
 
-var blackFridayPrice = dealFinder (originalPrice, percentDiscount);
-	function dealFinder() {
+var blackFridayPrice = dealFinder (prompt("What is the original price of the item? \($xx.xx\)"), prompt("What is the percentage off the original price of the item? \(XX\))"));
+	function dealFinder(originalPrice, percentDiscount) {
 		var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount)/100));
 		return finalPrice;
 	}
-
-var originalPrice = prompt("What is the original price of the item? \($xx.xx\)");
-var percentDiscount = prompt("What is the percentage off the original price of the item? \(XX\))");
 
 console.log("The sale price of your item is $" + Number(blackFridayPrice) + ".");
