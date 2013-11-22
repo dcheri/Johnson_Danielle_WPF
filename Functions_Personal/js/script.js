4032b2c729652fd179ae36d63ebf4d4e0d8741a7
dcheri <dcheri@fullsail.edu>
2013-11-21 22:33:02 -0600

    Defined variables to obtain user input.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,5 +1,8 @@
 // Danielle Johnson | November 21, 2013 | Functions_Personal
 
+var originalPrice = prompt("What is the original price of the item? \($xx.xx\)");
+var percentDiscount = prompt("What is the percentage off the original price of the item? \(XX\))");
+
 var blackFridayPrice = function dealFinder ( originalPrice, percentDiscount) {
 	var finalPrice = originalPrice - (originalPrice * ( Number(percentDiscount) / 100 ));
 	return finalPrice;
